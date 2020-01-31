720abeba88993df0d4804ccc4ad32888ec50ea38
Luis@mipc <oneillvilla@gmail.com>
2020-01-31 06:16:57 -0400

    Database amd css

---
+++ proyecto-php-poo/database/tienda_master.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE tienda_master;
+USE tienda_master;
+
+CREATE TABLE usuarios(
+id_usuario 	int(255) auto_increment not null,
+nombre	    varchar(255) not null,
+apellidos	varchar(255),
+email		varchar(255) not null,
+password	varchar(255) not null,
+role		varchar(20),
+imagen		varchar(255),
+CONSTRAINT pk_usuarios PRIMARY KEY(id_usuario),
+CONSTRAINT uq_email UNIQUE(email)
+)ENGINE=InnoDb;
+
+INSERT INTO usuarios VALUES(NULL, 'Admin', 'Admin', 'admin@admin.com', '12345', 'admin', null);
+
+CREATE TABLE categorias(
+id_categoria 	int(255) auto_increment not null,
+nombre	    	varchar(255) not null,
+CONSTRAINT pk_categorias PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
+
+INSERT INTO categorias VALUES(NULL, 'Manga Corta');
+INSERT INTO categorias VALUES(NULL, 'Tirantes');
+INSERT INTO categorias VALUES(NULL, 'Manga Larga');
+INSERT INTO categorias VALUES(NULL, 'Sudadera');
+
+CREATE TABLE productos(
+id_producto 		int(255) auto_increment not null,
+descripcion	    	text,
+precio				float(100,2) not null,
+fk_id_categoria		int(255) not null,
+stock				int(255) not null,
+oferta				varchar(2),
+fecha				date not null,
+imagen				varchar(255),
+CONSTRAINT pk_producto PRIMARY KEY(id_producto),
+CONSTRAINT fk_producto_categoria FOREIGN KEY(fk_id_categoria) REFERENCES categorias(id_categoria)
+)ENGINE=InnoDb;
+
+CREATE TABLE pedidos(
+id_pedido		int(255) auto_increment not null,
+fk_id_usuario	int(255) not null,
+provincia		varchar(100) not null,
+localidad 		varchar(100) not null,
+direccion		varchar(255) not null,
+coste 			float(250,2) not null,
+estado			varchar(255) not null,
+fecha			date,
+hora 			time,
+
+CONSTRAINT pk_pedidos PRIMARY KEY(id_pedido),
+CONSTRAINT fk_pedido_usuario FOREIGN KEY(fk_id_usuario) REFERENCES usuarios(id_usuario)
+)ENGINE=InnoDb;
+
+CREATE TABLE lineas_pedidos(
+id_lp		int(255) auto_increment not null,
+fk_id_pedido		int(255) not null,
+fk_id_producto		int(255) not null,
+
+CONSTRAINT pk_lineas_pedidos PRIMARY KEY(id_lp),
+CONSTRAINT fk_linea_pedido FOREIGN KEY(fk_id_pedido) REFERENCES pedidos(id_pedido),CONSTRAINT fk_linea_producto FOREIGN KEY(fk_id_producto) REFERENCES productos(id_producto)
+)ENGINE=InnoDb;
---
+++ proyecto-php-poo/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Tienda de camisetas</title>
+		<link rel="stylesheet" href="assets/css/styles.css">
+	</head>
+	<body>
+		<div id="container">
+			<!-- Cabecera -->
+			<header id="header" class="">
+				<div id="logo">
+					<img src="assets/images/camisa-blanca-dibujo-png.png" alt="Imagen_logo">
+					<a href="index.php">
+						Tienda de camisetas
+					</a>
+				</div>
+			</header>
+			<!-- /header -->
+			<!-- Menú -->
+			<nav id="menu">
+				<ul>
+					<li><a href="#" title="">Inicio</a></li>
+				</ul>
+				<ul>
+					<li><a href="#" title="">Cat 1</a></li>
+				</ul>
+				<ul>
+					<li><a href="#" title="">Cat 2</a></li>
+				</ul>
+				<ul>
+					<li><a href="#" title="">Cat 3</a></li>
+				</ul>
+			</nav>
+			<!-- Barra lateral -->
+			<div id="content">
+				<aside id="lateral">
+					<div id="login" class="log_aside">
+						<form action="#" method="post" accept-charset="utf-8">
+							<label for="email">Email</label>
+							<input type="text" name="email" value="">
+							<label for="password">Password</label>
+							<input type="text" name="password" value="">
+							<input type="submit" name="enviar" value="Enviar">
+						</form>
+						<a href="#">Mis Pedidos</a>
+						<a href="#">Gestionar Pedidos</a>
+						<a href="#">gestionar Categorias</a>
+						
+					</div>
+				</aside>
+			</div>
+			<!-- Contenido central -->
+			<div id="cental">
+				<div class="product">
+					<img src="assets/images/camisa-blanca-dibujo-png.png" alt="">
+					<h2>Camiseta Azul holgada Ancha</h2>
+					<p>30 Euros</p>
+					<a href="">Comprar</a>
+				</div>
+				<div class="product">
+					<img src="assets/images/camisa-blanca-dibujo-png.png" alt="">
+					<h2>Camiseta Azul holgada Ancha</h2>
+					<p>30 Euros</p>
+					<a href="">Comprar</a>
+				</div>
+				<div class="product">
+					<img src="assets/images/camisa-blanca-dibujo-png.png" alt="">
+					<h2>Camiseta Azul holgada Ancha</h2>
+					<p>30 Euros</p>
+					<a href="">Comprar</a>
+				</div>
+			</div>
+			<!-- Footer ó pie de pagina -->
+			<footer id="footer">
+				<p>Desarrollado por Luis Hurtado &copy; 2020</p>
+			</footer>
+		</div>
+		
+	</body>
+</html>
